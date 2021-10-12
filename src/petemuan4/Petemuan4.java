923c57e9a42a99245b34f18dde018140a1bb70cd
herisvan321 <herisvan321@gmail.com>
2021-10-12 10:01:06 +0700

    Pertemuan 4

---
+++ src/petemuan4/Petemuan4.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package petemuan4;
+
+import ui.Home;
+
+/**
+ *
+ * @author user
+ */
+public class Petemuan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home fHome = new Home();
+        fHome.setVisible(true);
+    }
+    
+}
